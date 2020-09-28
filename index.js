09044b4dd1bfdfbbeff2f38a3a79a8fe98ba9ba9
samira-estes-garcia <samira.estes.garcia@gmail.com>
2020-09-28 12:14:01 -0500

    mobile nav close on clickoutside

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 console.log("Connected");
 
+//index and about
 $(".hamburger").click(function () {
   $(".nav-menu-mobile").toggle();
-  $(".nav-menu-mobile").toggleClass("show");
+  $(".nav-menu-mobile").addClass("show");
+});
+
+//mobile nav close on click outside index.html
+$(".hero-text-container").click(function () {
+  $(".nav-menu-mobile").hide();
+});
+
+$("#work").click(function () {
+  $(".nav-menu-mobile").hide();
+});
+
+$("footer").click(function () {
+  $(".nav-menu-mobile").hide();
+});
+
+//mobile nav close on click outside about.html
+$(".hero").click(function () {
+  $(".nav-menu-mobile").hide();
+});
+
+$(".info-paragraphs").click(function () {
+  $(".nav-menu-mobile").hide();
+});
+
+$("footer").click(function () {
+  $(".nav-menu-mobile").hide();
 });
